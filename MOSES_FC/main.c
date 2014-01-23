6197b2cceef069fc35b75e8e2a06a0f5849224c8
byrdie <roytsmart@gmail.com>
2014-01-22 17:23:16 -0700

    Beginning repo

---
+++ MOSES_FC/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: byrdie
+ *
+ * Created on January 22, 2014, 5:21 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
