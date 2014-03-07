5eff2e34dcf7d670fb10ee71a36d6a71d81c5a2e
byrdie <byrdie@UNIVAC>
2014-03-07 12:49:08 -0700

    Tried altering select arguments to make it return.

--- MOSES_FC/packet.c
+++ MOSES_FC/packet.c
@@ -199,7 +199,7 @@ int readData(int fd, char * data, int len) {
         gettimeofday(&now, NULL);
         timeToWait.tv_sec = now.tv_sec + 2;
         timeToWait.tv_usec = 0;
-        available = select(1, &set, NULL, NULL, &timeToWait);   //Use select to be able to exit, and not hang on read()
+        available = select(fd+1, &set, NULL, NULL, &timeToWait);   //Use select to be able to exit, and not hang on read()
         if(available) { //If select returns true, read the data
             int rsz = read(fd, data, len);
             while (rsz < len) {
