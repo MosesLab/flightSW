89b5e9b78b1d415722a169de7e02c7f491936c6f
birdie <birdie@teslaCoil>
2014-03-24 12:58:50 -0600

    fix temp_failure_retry

--- MOSES_FC/packet.c
+++ MOSES_FC/packet.c
@@ -241,5 +241,5 @@ int input_timeout(int filedes, unsigned int seconds) {
     timeout.tv_usec = 0;
 
     /*select returns 0 if timeout, 1 if input data is avaialble, -1 if error*/
-    return TEMP_FAILURE_RETRY(select(FD_SETSIZE, &set, NULL, NULL, &timeout));
+    return TEMP_FAILURE_RETRY (select(FD_SETSIZE, &set, NULL, NULL, &timeout));
 }
