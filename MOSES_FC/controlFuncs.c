ef85fe0ba3d8d0bea5f8b02cbe163e60663d9eed
birdie <birdie@teslaCoil>
2014-05-15 12:51:10 -0600

    fixed the way the packet constructor copies the packet data

--- MOSES_FC/controlFuncs.c
+++ MOSES_FC/controlFuncs.c
@@ -1,1 +1,13 @@
+/*
+ * controlFuncs.c
+ * 
+ * Author: Roy Smart
+ * Created: May 15, 2014
+ * 
+ * contains the functions that direct the flight computer what to do in the 
+ * event of user input, e.g. timers, uplinks and hlp packets.
+ */
 
+int hlpUplink (Packet * p){
+   
+}
--- MOSES_FC/controlFuncs.h
+++ MOSES_FC/controlFuncs.h
@@ -11,7 +11,31 @@
 #ifndef CONTROLFUNCS_H
 #define	CONTROLFUNCS_H
 
+#include "system.h"
+#include "hlp.h"
 
+/*hlp control functions*/
+int hlpUplink(Packet*);
+
+/*Timer control functions*/
+void tDataStart();
+void tDataStop();
+void tDark2();
+void tDark4();
+void tSleep();
+
+/*uplink control functions*/
+void uplinkInit();
+
+void uDataStart();
+void uDataStop();
+void uDark1();
+void uDark2();
+void uDark3();
+void uDark4();
+void uSleep();
+void uWake();
+void uTest();
 
 #endif	/* CONTROLFUNCS_H */
 
