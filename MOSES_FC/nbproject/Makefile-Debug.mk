a972a5e6b21cc877a92db747ec470665fa6ca7d8
birdie <birdie@teslaCoil>
2014-05-15 12:56:02 -0600

    modified makefile

--- MOSES_FC/nbproject/Makefile-Debug.mk
+++ MOSES_FC/nbproject/Makefile-Debug.mk
@@ -36,7 +36,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 # Object Files
 OBJECTFILES= \
 	${OBJECTDIR}/control.o \
-	${OBJECTDIR}/hkdown.o \
+	${OBJECTDIR}/controlFuncs.o \
 	${OBJECTDIR}/lockingQueue.o \
 	${OBJECTDIR}/main.o \
 	${OBJECTDIR}/packet.o
@@ -71,10 +71,10 @@ ${OBJECTDIR}/control.o: control.c
 	${RM} $@.d
 	$(COMPILE.c) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/control.o control.c
 
-${OBJECTDIR}/hkdown.o: hkdown.c 
+${OBJECTDIR}/controlFuncs.o: controlFuncs.c 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.c) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/hkdown.o hkdown.c
+	$(COMPILE.c) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/controlFuncs.o controlFuncs.c
 
 ${OBJECTDIR}/lockingQueue.o: lockingQueue.c 
 	${MKDIR} -p ${OBJECTDIR}
