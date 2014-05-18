efc88d21d5c82055f85b35b5342fc7008c99031c
byrdie <byrdie@BATTLESTATION>
2014-05-17 18:37:43 -0600

    Fixing compiler errors in hashtable

--- MOSES_FC/hashTable.c
+++ MOSES_FC/hashTable.c
@@ -14,6 +14,12 @@
 #define HASHSIZE 101
 
 
+/*functions*/
+unsigned hash(char*);
+Node* lookup(Node**, char*);
+Node* install(Node**, char*, char*);
+char* strduplicate(char*);
+
 /*hash: form hash value for string s*/
 unsigned hash(char* s){
     unsigned hashval;
@@ -34,23 +40,13 @@ Node* lookup(Node** hTable, char* s){
     return NULL;        //Not Found
 }
 
-/*Make a duplicate of string s*/
-char* strdup(char* s){
-    char* p;
-    p = (char*)malloc(strlen(s)+1);
-    if(p != NULL){
-        strcpy(p,s);
-    }
-    return p;
-}
-
 /*install: put name and definition in hashtable*/
 Node* install(Node** hTable, char* name, char* defn){
     Node* np;
     unsigned hashval;
     if((np = lookup(hTable, name)) == NULL){    //Not found
         np = (Node*) malloc(sizeof(*np));
-        if(np == NULL || (np->name = strdup(name)) == NULL){
+        if(np == NULL || (np->name = strduplicate(name)) == NULL){
             return NULL;
         }
         hashval = hash(name);
@@ -60,11 +56,19 @@ Node* install(Node** hTable, char* name, char* defn){
     else{       //Already in hashtable
         free((void*) np->defn); //Free previous definition
     }
-    if((np->defn = strdup(defn)) == NULL){
+    if((np->defn = strduplicate(defn)) == NULL){
         return NULL;
     }
     return np;  
 }
 
-
+/*Make a duplicate of string s*/
+char* strduplicate(char* s){
+    char* p;
+    p = (char*)malloc(strlen(s)+1);
+    if(p != NULL){
+        strcpy(p,s);
+    }
+    return p;
+}
 
