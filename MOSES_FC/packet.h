6b053fff47bc3150e542240de365eac00db3b7ce
birdie <birdie@teslaCoil>
2014-06-30 09:23:16 -0600

    fixing compiler errors on VDX

--- MOSES_FC/packet.h
+++ MOSES_FC/packet.h
@@ -25,8 +25,8 @@
 #define HKDOWN_REAL "/dev/ttyS1"
 //#define HKUP "/dev/ttyUSB0"
 //#define HKDOWN "/dev/ttyUSB1"
-#define HKUP_SIM "/dev/pts/2"
-#define HKDOWN_SIM "/dev/pts/6"
+#define HKUP_SIM "/dev/pts/6"
+#define HKDOWN_SIM "/dev/pts/8"
 
 #define HKUP 1
 #define HKDOWN 0
