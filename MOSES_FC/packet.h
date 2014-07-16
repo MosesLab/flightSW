b7081b808938c68aece9cfe03cf206b053372e73
birdie <birdie@teslaCoil>
2014-07-16 11:20:10 -0600

    tryint to fix program exit

--- MOSES_FC/packet.h
+++ MOSES_FC/packet.h
@@ -25,8 +25,8 @@
 #define HKDOWN_REAL "/dev/ttyS3"
 //#define HKUP "/dev/ttyUSB0"
 //#define HKDOWN "/dev/ttyUSB1"
-#define HKUP_SIM "/dev/pts/3"
-#define HKDOWN_SIM "/dev/pts/6"
+#define HKUP_SIM "/dev/pts/8"
+#define HKDOWN_SIM "/dev/pts/10"
 
 #define HKUP 1
 #define HKDOWN 0
