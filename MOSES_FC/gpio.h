a72d359a4ba38e3002024b0dac531f16e0c29ec9
birdie <birdie@teslaCoil>
2014-07-18 10:30:02 -0600

    updated power pins to test gpio output

--- MOSES_FC/gpio.h
+++ MOSES_FC/gpio.h
@@ -27,17 +27,18 @@
 
 /*Power macros*/
 #define NUM_SUBSYSTEMS 10
-#define POWER_OFFSET 0x2C
-#define SHUTTER_SIM 0x2
-#define ROE_SIM 0x4
-#define PREMOD_SIM 0x8
-#define TCS_1_SIM 0x10
-#define TCS_3_SIM 0x20
-#define TCS_2_SIM 0x40
-#define TCS_SIM 0x80
-#define REG_5V_SIM 0x100
-#define REG_12V_SIM 0x200
-#define H_ALPHA_SIM 0x400
+//#define POWER_OFFSET 0x2C
+#define POWER_OFFSET 0x10
+#define SHUTTER_SIM 0x40000
+#define ROE_SIM 0x80000
+#define PREMOD_SIM 0x1000000
+#define TCS_1_SIM 0x2000000
+#define TCS_3_SIM 0x4000000
+#define TCS_2_SIM 0x8000000
+#define TCS_SIM 0x10000000
+#define REG_5V_SIM 0x20000000
+#define REG_12V_SIM 0x40000000
+#define H_ALPHA_SIM 0x80000000
 
 /*GPIO write variables*/
 PLX_DEVICE_OBJECT fpga_dev;
