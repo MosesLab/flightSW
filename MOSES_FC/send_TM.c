a3ae73a17e198d356ac0f3bef7ba03c85dff2917
jacksonremington@yahoo.com <moses@MOSES-EGSE>
2015-03-12 15:41:14 -0600

    Updated, formatted, cleaned - ready for test release ( send_TM only )

--- MOSES_FC/send_TM.c
+++ MOSES_FC/send_TM.c
@@ -2,22 +2,20 @@
 /********************************************************************************
  * MOSES telemetry downlink test code
  *
- * Author: Jacob Plovanic, Roy Smart, Jackson Remington
+ * Author: Jackson Remington, Roy Smart, Jake Plavonic
  * History:
  *  Created Dec 17 2013
  *  Tested  Dec 18 2013 
- *  Tested May 9 2014 successfully at White Sands Missile Range (See results below)
+ *  Tested  May 9 2014 successfully at White Sands Missile Range (See results below)
+ *  Tested  March 10 2015 successfully and integrated into flightSW
  *
  * Uses the Microgate USB SyncLink adapter to achieve synchronous serial speeds
- * of 10 Mbps to downlink MOSES science data. The data are contained in 16 MB 
- * (possibly 12 MB) files.
+ * of 10 Mbps to downlink MOSES science data. The data are contained in 12 MB files.
  *
  * This program will configure the SyncLink device for use as a linux serial node
- * with the appropriate framing, bitrate, desired error checking, idle pattern,
- * and preamble. It will then send out the 16 MB .roe file. As a first step, we 
- * will use this program to test the timing and capabilities of sending entire
- * MOSES images through the telemetry, since there might be delays associated with
- * the USB transfer. We can then fold this program into the main flight software
+ * with the appropriate framing, bit rate, desired error checking, idle pattern,
+ * and preamble. It will then send out the 12 MB .roe file. This program was 
+ * developed as stand-along before being implemented into the main flight software
  * program.
  *
  * Required files:
@@ -65,8 +63,6 @@
  *
  ******************************************************************************/
 
-
-
 #include "send_TM.h"
 #include "synclink.h"
 
@@ -85,46 +81,58 @@ void display_usage(void) {
             "Default is /dev/ttyUSB0)\n");
 }
 
+/* Function to (de)initialize synclink before send files */
 int synclink_init(int killSwitch) {
-    /* 
-     * Fork and exec the fsynth program to set the clock source on the SyncLink
-     * to use the synthesized 20 MHz clock from the onboard frequency synthesizer
-     * chip, for accurate generation of a 10 Mbps datastream. fsynth needs to be
-     * in the PATH. 
-     */
-
     int rc, fd = 0;
     int sigs, idle;
     char *devname = "/dev/ttyUSB0";
     char msg[255];
 
+    if (killSwitch == 1) { 
+        /* Then we are shutting down the synclink */
+        record("synclink killSwitch: Turn off RTS and DTR\n");
+        sigs = TIOCM_RTS + TIOCM_DTR;
+        rc = ioctl(fd, TIOCMBIC, &sigs);
+        if (rc < 0) {
+            sprintf(msg, "negate DTR/RTS error=%d %s\n", errno, strerror(errno));
+            record(msg);
+            return rc;
+        }
 
-    if (killSwitch == 0) {
-
-
+        /* Close the device*/
+        close(fd);
+    }
+    else if (killSwitch == 0) {
+        /* Then we are preparing the synclink */
         int ldisc = N_HDLC, child_status;
         MGSL_PARAMS params;
 
+        /* 
+         * Fork and exec the fsynth program to set the clock source on the SyncLink
+         * to use the synthesized 20 MHz clock from the onboard frequency synthesizer
+         * chip, for accurate generation of a 10 Mbps datastream. fsynth needs to be
+         * in the PATH. 
+         */
         pid_t pid = fork();
         if (pid == -1) { //Check Fork
             perror("Fork failure");
             exit(EXIT_FAILURE);
         }
-
         if (pid == 0) {
-            execlp("fsynth", "fsynth", devname, (char *) NULL); //fsynth was compiled with 20MHz
-            perror("execlp"); //selected in code
-            _exit(EXIT_FAILURE); //Child should die after exec call. If it gets
-            //here then the exec failed
+            /* Child should die after exec call. If it gets here then the exec failed*/
+            /* fsynth was compiled with 20MHz */
+            execlp("fsynth", "fsynth", devname, (char *) NULL);
+            perror("execlp");
+            _exit(EXIT_FAILURE); 
         } else if (pid > 0) {
-            wait(&child_status); //Wait for child to finish
+            wait(&child_status);
         }
 
         sprintf(msg, "send HDLC data on %s\n", devname);
         record(msg);
 
         /* open serial device with O_NONBLOCK to ignore DCD input */
-        fd = open(devname, O_RDWR | O_NONBLOCK, 0); // Changed from 'O_TRUNC' to 'O_RDWR | O_NONBLOCK'
+        fd = open(devname, O_RDWR | O_NONBLOCK, 0);
         if (fd < 0) {
             sprintf(msg, "open error=%d %s\n", errno, strerror(errno));
             record(msg);
@@ -166,14 +174,13 @@ int synclink_init(int killSwitch) {
          * Output 10000000bps clock on auxclk output
          * No hardware CRC
          */
-
-        params.mode = MGSL_MODE_HDLC;
-        params.loopback = 0;
-        params.flags = HDLC_FLAG_RXC_RXCPIN + HDLC_FLAG_TXC_BRG;
-        params.encoding = HDLC_ENCODING_NRZ;
-        params.clock_speed = 10000000;
-        params.crc_type = HDLC_CRC_16_CCITT;
-        params.preamble = HDLC_PREAMBLE_PATTERN_ONES;
+        params.mode            = MGSL_MODE_HDLC;
+        params.loopback        = 0;
+        params.flags           = HDLC_FLAG_RXC_RXCPIN + HDLC_FLAG_TXC_BRG;
+        params.encoding        = HDLC_ENCODING_NRZ;
+        params.clock_speed     = 10000000;
+        params.crc_type        = HDLC_CRC_16_CCITT;
+        params.preamble        = HDLC_PREAMBLE_PATTERN_ONES;
         params.preamble_length = HDLC_PREAMBLE_LENGTH_16BITS;
 
         /* set current device parameters */
@@ -197,7 +204,7 @@ int synclink_init(int killSwitch) {
 
         /* set device to blocking mode for reads and writes */
         fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) & ~O_NONBLOCK);
-
+        
         record("Turn on RTS and DTR serial outputs\n");
         sigs = TIOCM_RTS + TIOCM_DTR;
         rc = ioctl(fd, TIOCMBIS, &sigs);
@@ -208,66 +215,59 @@ int synclink_init(int killSwitch) {
             return rc;
         }
 
-        /*enable transmitter; should this appear before "return"?*/
+        /*enable transmitter*/
         int enable = 1;
         rc = ioctl(fd, MGSL_IOCTXENABLE, enable);
-    } else if (killSwitch == 1) { //Turns off synclink
-        record("synclink killSwitch: Turn off RTS and DTR\n");
-        sigs = TIOCM_RTS + TIOCM_DTR;
-        rc = ioctl(fd, TIOCMBIC, &sigs);
-        if (rc < 0) {
-            sprintf(msg, "negate DTR/RTS error=%d %s\n", errno, strerror(errno));
-            record(msg);
-            return rc;
-        }
+    } 
 
-        /* Close the device*/
-        close(fd);
-
-    }
-
+    /* exit with synclink's file pointer */
     return fd;
 }
 
+/* Program to send passed images through synclink */
 int send_image(roeimage_t * image, int fd) {
-
+    /* 
+     * Write file to TM. This requires input from memory in the form of roeimage_t struct, 
+     * then sending the xml and image buffers to the device via a write call.
+     */
     int rc = 0;
     int i;
     int totalSize = 0;
-    char *imagename = NULL; //Number to determine how much data is sent
+    char *imagename = NULL;
     struct timeval time_begin, time_end;
     int time_elapsed;
     char msg[255];
 
-    /* 
-     * Write file to TM. This requires input from memory in the form of roeimage_t OR xml_t struct, 
-     * then sending the data buffer to the device via a write call.
-     */
-
+    /* Main loop - enter and exit with each image */
     if (ts_alive) {
 
         record("Sending data: \n");
         
-        /*start sending imageindex.xml snippets*/
-        record("XML File...\n");
+        /* start sending imageindex.xml snippets */
+        record("Sending XML File...\n");
         totalSize = 0;
         imagename = "imageindex.xml";
-        gettimeofday(&time_begin, NULL); //Determine elapsed time for xml file write to TM
+        
+        /*Determine elapsed time for xml file write to TM*/
+        gettimeofday(&time_begin, NULL); 
 
-        /*write xml lines*/
+        /*write xml snippets*/
         for (i = 0; i < (image->xml_cur_index + 1); i++) {
             rc = write(fd, xml_snips[i], xml_snips_sz[i]);
             if (rc < 0) {
                 sprintf(msg, "write error=%d %s\n", errno, strerror(errno));
                 record(msg);
+                break;
             }
+            else totalSize += rc;
+            
             /* block until all data sent */
-            totalSize += rc;
             rc = tcdrain(fd);
         }
 
-        if (rc < 0) return rc; //Finishes the write error handling after the break
-
+        /*Finishes the write error handling after the break*/
+        if (rc < 0) return rc;
+        
         /*write terminating characters*/
         rc = write(fd, imagename, strlen(imagename));
         if (rc < 0) {
@@ -275,11 +275,12 @@ int send_image(roeimage_t * image, int fd) {
             record(msg);
             return rc;
         }
+        
         /*block until all data sent*/
         rc = tcdrain(fd);
 
         /*report time taken for XML file*/
-        gettimeofday(&time_end, NULL); //Timing
+        gettimeofday(&time_end, NULL);
         record("all data sent\n");
         sprintf(msg, "Sent %d bytes of data from file %s.\n", totalSize, imagename);
         record(msg);
@@ -287,44 +288,43 @@ int send_image(roeimage_t * image, int fd) {
                 + (long) (time_end.tv_usec) - (long) (time_begin.tv_usec);
         sprintf(msg, "Time elapsed: %-3.2f seconds.\n", (float) time_elapsed / (float) 1000000);
         record(msg);
-        
-        
-        
 
-        /*start sending science data*/
+        /*prepare to send science data*/
         record("ROE File...\n");
-        totalSize = 0;  // reset variable that keeps track of bytes sent
+        totalSize = 0;
         imagename = image->filename;
-        gettimeofday(&time_begin, NULL); //Determine elapsed time for file write to TM
+        gettimeofday(&time_begin, NULL);
         
         /*Write image frames here*/
         for (i = 1; i < 4; i++) {
-            rc = write(fd, image->data[i], image->size[i] * 2); // size corresponds to num pixels, so multiply by 2 to get bytes
+            /* size corresponds to num pixels, so multiply by 2 to get bytes */
+            rc = write(fd, image->data[i], image->size[i] * 2);
             if (rc < 0) {
                 sprintf(msg, "write error=%d %s\n", errno, strerror(errno));
                 record(msg);
                 break;
             }
+            else totalSize += rc;
+            
             /* block until all data sent */
-            totalSize += rc;
             rc = tcdrain(fd);
-
         }
-        if (rc < 0) return rc; //Finishes the write error handling after the break
+        
+        /*Finishes the write error handling after the break*/
+        if (rc < 0) return rc; 
 
-        /*write terminating characters*/
-        sprintf(msg, "Sending image file %s \n", imagename);
-        record(msg);
-        rc = write(fd, imagename + 7, strlen(imagename) - 7); //Ending characters "YYMMDDHHmmss.roe"
+        /*write terminating characters after removing dir path*/
+        rc = write(fd, imagename + 7, strlen(imagename) - 7);
         if (rc < 0) {
             sprintf(msg, "write error=%d %s\n", errno, strerror(errno));
             record(msg);
             return rc;
         }
+        
         /*block until all data sent*/
         rc = tcdrain(fd);
 
-        gettimeofday(&time_end, NULL); //Timing
+        gettimeofday(&time_end, NULL);
         record("all data sent\n");
         sprintf(msg, "Sent %d bytes of data from file %s.\n", totalSize, imagename);
         record(msg);
@@ -333,10 +333,7 @@ int send_image(roeimage_t * image, int fd) {
         sprintf(msg, "Time elapsed: %-3.2f seconds.\n", (float) time_elapsed / (float) 1000000);
         record(msg);
     }
-
-
-
+    
+    /* return success marker */
     return 1;
-
-
 }
