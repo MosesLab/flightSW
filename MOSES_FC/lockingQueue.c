c989430cb4255203f65c192144689d4aa87456a4
Roy Smart <roytsmart@gmail.com>
2015-06-29 17:46:00 -0600

    Changed action of shutting down flight software. -Normal shutdown should cancel threads before killing bash. -Sleep shutdown should continue writing images until complete and then shut down flight computer ---> Add new variable img_wr_alive to make sure that image writer starts cleaning up
    Don't mess with the goddamn locking queues!

--- MOSES_FC/lockingQueue.c
+++ MOSES_FC/lockingQueue.c
@@ -56,7 +56,10 @@ void * dequeue(LockingQueue * queue) {
     /*The thread must be locked for pthread_cond_timedwait() to work*/
     pthread_mutex_lock(&queue->lock);
 //    while ((queue->count == 0)) {
+    if(queue->count == 0){
         pthread_cond_wait(&queue->cond, &queue->lock);
+    }
+        
 //    }
 
     /*check if program is still active*/
